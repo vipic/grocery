573f42fe609a2c3a0f3e9e67b9324fb4918e98bd
vipic <read_xms@hotmail.com>
2022-10-11 13:26:11 +0800

    fix: fix the word spell

--- Scripts/Reddit.js
+++ Scripts/Reddit.js
@@ -6,5 +6,5 @@ let obj = JSON.parse(body);
 const children = obj.data.children.filter(item=>{
     return item.data.priority_id === undefined
 })
-obj.data.childrend = children
+obj.data.children = children
 $done({ body: JSON.stringify(obj) });
