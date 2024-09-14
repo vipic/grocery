6a2e79d23261b9df85f8135339db5d761e6fefba
vipic <read_xms@hotmail.com>
2024-09-14 10:59:01 +0800

    update

--- Scripts/bilibili_pc.js
+++ Scripts/bilibili_pc.js
@@ -7,7 +7,7 @@ try {
     const tmp = obj.data.list.filter(inner => {
       const filterList= ['美食', '法律', '车', '新能源', '美妆', '同人', 'cos', "mv", '翻唱', '三农']
           //如果tanme符合filterList中的任意一个，则返回false，否则返回true
-      return filterList.some(item => inner.tname.includes(item) === false);
+      return filterList.every(item => inner.tname.includes(item) === false);
     });
     $done({
       body: JSON.stringify({
