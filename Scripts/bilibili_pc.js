dc281ca4d0f81438d83a93fbad3d79c20021244e
vipic <read_xms@hotmail.com>
2024-09-19 10:19:16 +0800

    update

--- Scripts/bilibili_pc.js
+++ Scripts/bilibili_pc.js
@@ -2,22 +2,35 @@
 
 const url = $request.url;
 try {
-  if(url.includes('popular1')) {
-    let obj = JSON.parse($response.body);
-    const tmp = obj.data.list.filter(inner => {
-      const tagFilterList= ['美食', '法律', '车', '新能源', '美妆', '同人', 'cos', "mv", '翻唱', '三农']
-      const desFiterKeywords = ['黑神话']
-          //如果tanme符合ftagFilterList中的任意一个，则返回false，否则返回true
-      return tagFilterList.every(item => inner.tname.includes(item) === false) && desFiterKeywords.every(item=> inner.desc.includes(item) === false);
+  if(url.includes('popular')) {
+    const body = JSON.parse($response.body);
+    const raw = body.data.list;
+
+    const tmp = raw.filter(inner => {
+      const tagFilterList = [
+        "美食",
+        "法律",
+        "车",
+        "新能源",
+        "美妆",
+        "同人",
+        "cos",
+        "mv",
+        "翻唱",
+        "三农",
+        "特摄"
+      ];
+      const desFiterKeywords = ["黑神话","天命"];
+      //如果tanme符合ftagFilterList中的任意一个，则返回false，否则返回true
+      return (
+          tagFilterList.every(item => inner.tname.includes(item) === false) &&
+          desFiterKeywords.every(item => inner.desc.includes(item) === false) &&
+          desFiterKeywords.every(item => inner.title.includes(item) === false)
+      );
     });
+    body.data.list = tmp;
     $done({
-      body: JSON.stringify({
-        ...obj,
-        data: {
-          ...obj.data,
-          list: tmp,
-        },
-      }),
+      body: JSON.stringify(body),
     });
   } else {
     let obj = JSON.parse($response.body);
