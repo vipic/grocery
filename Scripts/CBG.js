881f47aeb4b4fb55b662c3555e4cbfd96099bba1
vipic <read_xms@hotmail.com>
2025-04-11 22:24:39 +0800

    feat: delete the key of that obj

--- Scripts/CBG.js
+++ Scripts/CBG.js
@@ -1,15 +1,15 @@
 const url = $request.url;
 let obj = JSON.parse($response.body);
 
-$done({ body: JSON.stringify({
-  ...obj,
-  equip: {
-    ...obj.equip,
-    video_info: {},
-    video_list: [],
-    other_info:{
-        ...obj.equip.other_info,
-        report_score_data: null
-    }
-  }
-}) });
+const other_info = obj.equip.other_info;
+delete other_info.report_score_data;
+$done({
+  body: JSON.stringify({
+    ...obj,
+    equip: {
+      ...obj.equip,
+      video_info: {},
+      video_list: [],
+    },
+  }),
+});
