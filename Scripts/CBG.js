12bef2173377796e3c87724096621a332786573f
vipic <read_xms@hotmail.com>
2025-04-12 10:15:57 +0800

    feat: add has_score flag to equip object and remove report_score_data

--- Scripts/CBG.js
+++ Scripts/CBG.js
@@ -1,15 +1,12 @@
 const url = $request.url;
 let obj = JSON.parse($response.body);
 
-
-if (obj?.equip?.other_info) { // 检查 obj.equip.other_info 是否可访问
-  delete obj.equip.other_info.report_score_data;
-}
 $done({
   body: JSON.stringify({
     ...obj,
     equip: {
       ...obj.equip,
+      has_score: false,
       video_info: {},
       video_list: [],
     },
