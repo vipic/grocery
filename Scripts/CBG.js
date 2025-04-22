984b79b5e4c7a1cffa48c458f86ca77fef942ffd
vipic <read_xms@hotmail.com>
2025-04-22 12:19:36 +0800

    feat: add is_agent_role_keep_online flag to CBG object

--- Scripts/CBG.js
+++ Scripts/CBG.js
@@ -9,6 +9,7 @@ $done({
       has_score: false,
       allow_multi_order: false,
       is_support_instalment: false,
+      is_agent_role_keep_online: false,
       video_info: {},
       video_list: [],
     },
